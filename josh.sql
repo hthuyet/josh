cd50c8e47e7b0d480028728487d2b8be5f64601c
thuyetlv <thuyetlv@vnpt-technology.vn>
2016-11-14 14:26:52 +0700

    add db

---
+++ josh.sql
@@ -0,0 +1,287 @@
+/*
+SQLyog Ultimate v11.3 (64 bit)
+MySQL - 5.6.16 : Database - josh
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`josh` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `josh`;
+
+/*Table structure for table `activations` */
+
+DROP TABLE IF EXISTS `activations`;
+
+CREATE TABLE `activations` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `code` varchar(256) DEFAULT NULL,
+  `user_id` int(5) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `completed` int(1) DEFAULT '0',
+  `completed_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `activations` */
+
+insert  into `activations`(`id`,`code`,`user_id`,`updated_at`,`created_at`,`completed`,`completed_at`) values (1,'NCHVPSPAtnDrQSgeOaHfwvMLIbI7ZbAu',1,'2016-11-14 04:25:39','2016-11-14 04:25:39',1,'2016-11-14 04:25:39');
+
+/*Table structure for table `blog_categories` */
+
+DROP TABLE IF EXISTS `blog_categories`;
+
+CREATE TABLE `blog_categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `blog_categories` */
+
+/*Table structure for table `blog_comments` */
+
+DROP TABLE IF EXISTS `blog_comments`;
+
+CREATE TABLE `blog_comments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `blog_id` int(10) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `website` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `comment` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `blog_comments` */
+
+/*Table structure for table `blogs` */
+
+DROP TABLE IF EXISTS `blogs`;
+
+CREATE TABLE `blogs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `blog_category_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `views` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `blogs` */
+
+/*Table structure for table `booktests` */
+
+DROP TABLE IF EXISTS `booktests`;
+
+CREATE TABLE `booktests` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `author` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `booktests` */
+
+insert  into `booktests`(`id`,`title`,`author`,`description`,`created_at`,`updated_at`) values (1,'safsaf','2fdb','sdf','2016-11-14 05:32:08','2016-11-14 05:32:08');
+
+/*Table structure for table `files` */
+
+DROP TABLE IF EXISTS `files`;
+
+CREATE TABLE `files` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `mime` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `files` */
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`migration`,`batch`) values ('2016_11_14_042200_create_taggable_table',1),('2016_11_14_044554_create_booktests_table',2);
+
+/*Table structure for table `persistences` */
+
+DROP TABLE IF EXISTS `persistences`;
+
+CREATE TABLE `persistences` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `user_id` int(5) DEFAULT NULL,
+  `code` varchar(512) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `persistences` */
+
+insert  into `persistences`(`id`,`user_id`,`code`,`updated_at`,`created_at`) values (3,1,'SKpo5hbwBTt2JOeJi8vPIfipWu5HJ3SH','2016-11-14 04:37:00','2016-11-14 04:37:00');
+
+/*Table structure for table `role_users` */
+
+DROP TABLE IF EXISTS `role_users`;
+
+CREATE TABLE `role_users` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `role_id` int(5) DEFAULT NULL,
+  `user_id` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `role_users` */
+
+insert  into `role_users`(`id`,`updated_at`,`created_at`,`role_id`,`user_id`) values (1,'2016-11-14 04:25:39','2016-11-14 04:25:39',1,1);
+
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) DEFAULT NULL,
+  `slug` varchar(128) DEFAULT NULL,
+  `permissions` varchar(512) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `roles` */
+
+insert  into `roles`(`id`,`name`,`slug`,`permissions`,`updated_at`,`created_at`) values (1,'Admin','admin','{\"admin\":1}','2016-11-14 04:25:39','2016-11-14 04:25:39'),(2,'User','user',NULL,'2016-11-14 04:25:39','2016-11-14 04:25:39');
+
+/*Table structure for table `taggable_taggables` */
+
+DROP TABLE IF EXISTS `taggable_taggables`;
+
+CREATE TABLE `taggable_taggables` (
+  `tag_id` int(10) unsigned NOT NULL,
+  `taggable_id` int(10) unsigned NOT NULL,
+  `taggable_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `taggable_taggables` */
+
+/*Table structure for table `taggable_tags` */
+
+DROP TABLE IF EXISTS `taggable_tags`;
+
+CREATE TABLE `taggable_tags` (
+  `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `normalized` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `taggable_tags` */
+
+/*Table structure for table `tasks` */
+
+DROP TABLE IF EXISTS `tasks`;
+
+CREATE TABLE `tasks` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `finished` tinyint(4) NOT NULL DEFAULT '0',
+  `task_description` text COLLATE utf8_unicode_ci NOT NULL,
+  `task_deadline` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tasks` */
+
+/*Table structure for table `throttle` */
+
+DROP TABLE IF EXISTS `throttle`;
+
+CREATE TABLE `throttle` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `ip` varchar(25) DEFAULT NULL,
+  `user_id` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `throttle` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `email` varchar(512) DEFAULT NULL,
+  `password` varchar(512) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `bio` text,
+  `gender` varchar(255) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `pic` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `postal` varchar(255) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`name`,`email`,`password`,`updated_at`,`created_at`,`deleted_at`,`bio`,`gender`,`dob`,`pic`,`country`,`state`,`city`,`address`,`postal`,`first_name`,`last_name`,`last_login`) values (1,NULL,'admin@admin.com','$2y$10$Pc6YFYUlOm/kTZOtbnMTN.udk/MMqOv3neNLtOQDMbFTcm2xQJaky','2016-11-14 04:37:00','2016-11-14 04:25:39',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'John','Doe','2016-11-14 04:37:00');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
